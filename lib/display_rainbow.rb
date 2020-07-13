e98dcffe18dfcf57bd4dc76faae3b35804bb8d1e
Cannigia Laluw <cannigialaluw@gmail.com>
2020-07-13 04:31:49 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,11 @@
 def display_rainbow(colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-  puts "#{colors}"
+  puts "R: #{[0]}"
+  puts "O: #{[1]}"
+  puts "Y: #{[2]}"
+  puts "G: #{[3]}"
+  puts "B: #{[4]}"
+  puts "I: #{[5]}"
+  puts "V: #{[6]}"
 end
 
 display_rainbow
